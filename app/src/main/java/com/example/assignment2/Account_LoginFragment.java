687dbee75b7bd12bf6098e2678e6248a7c23edca
Yumi Go <yumigoyumi@gmail.com>
2022-11-28 10:43:48 +0000

    layout login page

--- app/src/main/java/com/example/assignment2/Account_LoginFragment.java
+++ app/src/main/java/com/example/assignment2/Account_LoginFragment.java
@@ -15,15 +15,15 @@ import java.util.List;
 
 public class Account_LoginFragment extends Fragment {
 
-    RecyclerView recyclerView;
-    List<RecyclerItem> itemsList = new ArrayList<>();
+//    RecyclerView recyclerView;
+//    List<RecyclerItem> itemsList = new ArrayList<>();
 
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initItems();
+//        initItems();
     }
 
     @Override
@@ -31,21 +31,21 @@ public class Account_LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_account_tab1_login, container, false);
 
-        recyclerView = rootView.findViewById(R.id.recyclerView);
-        initView();
+//        recyclerView = rootView.findViewById(R.id.recyclerView);
+//        initView();
 
         return rootView;
     }
 
 
-    private void initView() {
-        GridLayoutManager layoutManager = new GridLayoutManager(this.getContext(), 2);
-        recyclerView.setLayoutManager(layoutManager);
-        RecyclerAdapter adapter = new RecyclerAdapter(itemsList);
-        recyclerView.setAdapter(adapter);
-    }
+//    private void initView() {
+//        GridLayoutManager layoutManager = new GridLayoutManager(this.getContext(), 2);
+//        recyclerView.setLayoutManager(layoutManager);
+//        RecyclerAdapter adapter = new RecyclerAdapter(itemsList);
+//        recyclerView.setAdapter(adapter);
+//    }
 
-    private void initItems() {
+//    private void initItems() {
 //        itemsList.add(new RecyclerItem("Korean", "Hwangsaengga", "Jongno-gu", getString(R.string.food_hwangsaengga_address), 37.58015809025108, 126.98064099768929, getString(R.string.food_hwangsaengga_description), R.drawable.food_hwangsaengga));
 //        itemsList.add(new RecyclerItem("Korean", "Wooraeok", "Jung-gu", getString(R.string.food_wooraeok_address), 37.568217336091884, 126.9987131577966, getString(R.string.food_wooraeok_description), R.drawable.food_wooraeok));
 //        itemsList.add(new RecyclerItem("Asian","Tuk Tuk Noodle Thai","Mapo-gu", getString(R.string.food_tuktuknoodlethai_address), 37.56514737979818, 126.92398211118115, getString(R.string.food_tuktuknoodlethai_description), R.drawable.food_tuktuk_noodle_thai));
@@ -54,6 +54,6 @@ public class Account_LoginFragment extends Fragment {
 //        itemsList.add(new RecyclerItem("Asian","Kojima","Gangnam-gu", getString(R.string.food_kojima_address), 37.52592321620524, 127.04192450934431, getString(R.string.food_kojima_description), R.drawable.food_kojima));
 //        itemsList.add(new RecyclerItem("Fusion","Bawi Pasta Bar","Yongsan-gu", getString(R.string.food_bawipastabar_address), 37.53719674808367, 127.00113679216557, getString(R.string.food_bawipastabar_description), R.drawable.food_bawi_pasta_bar));
 //        itemsList.add(new RecyclerItem("Vegan","Maison Jo","Seocho-gu", getString(R.string.food_maisonjo_address), 37.48194890826757, 127.01010830382059, getString(R.string.food_maisonjo_description), R.drawable.food_maison_jo));
-    }
+//    }
 
 }
